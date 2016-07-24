7eb6c235632f4640bca4f5bd7fe154317c510e07
wayne jessen <mremeat@gmail.com>
2016-07-24 15:17:41 -0700

    added some fancy calcuations to the front page

--- iwantanewmbp.js
+++ iwantanewmbp.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
-  
-    var lastMbpUpdate = Date(2015, 05, 19)
-    $('#lastMbpUpdate').text(lastMbpUpdate)
 
+
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+    //Get current datetime with empty date object
+    var now = new Date();
+
+    var lastMbpUpdate = new Date(2015, 5, 19);
+    var lastYear = lastMbpUpdate.getFullYear();
+    var lastDay = days[lastMbpUpdate.getDay()];
+    var lastMonth = months[lastMbpUpdate.getMonth()];
+    var lastDate  = lastMbpUpdate.getDate();
+
+    var fullText = "The last time the macbook pro was updated was " 
+        + lastDay + " " + lastMonth + " " + lastDate + " " + lastYear + "." ;
+
+    var diffInDays = Math.floor((now - lastMbpUpdate)/(1000 * 60 * 60 * 24));
+    var diffInDaysText = "That is " + diffInDays + " days.";
+
+    $('#lastMbpUpdate').text(fullText);
+    $('#diffInDays').text(diffInDaysText);
 });
